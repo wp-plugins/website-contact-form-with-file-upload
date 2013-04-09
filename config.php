1177b3a82f7bc5478c0c4e3ac1b170e525061588
nmedia <nmedia@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-04-09 09:20:50 +0000

    Plugin is bein pushed in repository for first time
    git-svn-id: https://plugins.svn.wordpress.org/website-contact-form-with-file-upload/tags/1.0@694381 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ config.php
@@ -0,0 +1,36 @@
+<?php
+/*
+ * this file contains pluing meta information and then shared
+ * between pluging and admin classes
+ * 
+ * [1]
+ */
+
+$plugin_dir = 'nm-wp-contact-form-v1';
+
+$plugin_meta		= array('name'			=> 'NM Website Contact',
+							'shortname'		=> 'nm_webcontact',
+							'path'			=> WP_PLUGIN_DIR . DIRECTORY_SEPARATOR . $plugin_dir,
+							'url'			=> WP_PLUGIN_URL . DIRECTORY_SEPARATOR . $plugin_dir,
+							'db_version'	=> 1.1,
+							'logo'			=> WP_PLUGIN_URL . DIRECTORY_SEPARATOR . $plugin_dir . DIRECTORY_SEPARATOR . 'images' . DIRECTORY_SEPARATOR . 'logo.png',
+							'men_position'	=> 60);
+
+
+function get_plugin_meta(){
+	
+	global $plugin_meta;
+	
+	//print_r($plugin_meta);
+	
+	return $plugin_meta;
+}
+
+
+/*
+ * rendering that It is Pro
+ */
+function nm_webcontact_pro(){
+	
+	echo '<strong><a style="background: rgb(219, 89, 83);color: #fff;padding: 5px;text-decoration: none;" href="http://www.najeebmedia.com/n-media-website-contact-form-with-file-uploader/">'.__('GET PRO', 'nm_webcontact').'</a></strong>';
+}
