8c90c82e66cf0b7957a282e8d40e7117d4465fc9
nmedia <nmedia@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-10 06:39:43 +0000

    version 1.7 just addded.
    git-svn-id: https://plugins.svn.wordpress.org/website-contact-form-with-file-upload/tags/1.7@1196061 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ templates/admin/get-pro.php
@@ -0,0 +1,33 @@
+<?php
+/*
+ * this is just to explain PRO features
+ */
+
+$proFeatures = '<ol>';
+$proFeatures .= '<li>'.__('Get access to support forum and post your question instantly.', 'nm_webcontact').'</li>';
+$proFeatures .= '<li>'.__('Set upload file as Required', 'nm_webcontact').'</li>';
+$proFeatures .= '<li>'.__('Allow multiple file upload', 'nm_webcontact').'</li>';
+$proFeatures .= '<li>'.__('Save all enteries as Custom Post Type', 'nm_webcontact').'</li>';
+$proFeatures .= '<li>'.__('Add unlimited receiver\'s email', 'nm_webcontact').'</li>';
+$proFeatures .= '<li>'.__('Date picker field input', 'nm_webcontact').'</li>';
+$proFeatures .= '<li>'.__('Sections can be placed to split long forms', 'nm_webcontact').'</li>';
+$proFeatures .= '<li>'.__('Hidden field can be embedded into form with pre-set value', 'nm_webcontact').'</li>';
+$proFeatures .= '<li>'.__('Set Max. and Min. Checked options for checkbox', 'nm_webcontact').'</li>';
+$proFeatures .= '<li>'.__('Add custom css for each form', 'nm_webcontact').'</li>';
+$proFeatures .= '</ol>';
+
+$proFeatures .= '<br><a class="button-primary" href="http://www.najeebmedia.com/n-media-website-contact-form-with-file-uploader/">'.__('Get Pro','nm_webcontact').'</a><br><br>';
+
+$proFeatures .= '<p>';
+$proFeatures .= '<h2>'.__('Best Photo Editing Tool', 'nm_webcontact').'</h2>';
+$proFeatures .= __('This plugin can be used as best Photo Editing Tool to allow your users to upload and Edit Photos online using world\'s best Photo Editor \'Aviary\'. There is an Add-on for this','nm_webcontact');
+$proFeatures .= '<br><br><a class="button" target="_blank" href="http://www.aviary.com/">'.__('What is Aviary?','nm_webcontact').'</a>';
+$proFeatures .= ' <a class="button-primary" href="http://webcontact.wordpresspoets.com/">'.__('See Demo','nm_webcontact').'</a>';
+$proFeatures .= ' <a class="button-primary" href="http://webcontact.wordpresspoets.com/demo-of-photo-editing/">'.__('See Photo Editing Add-on Demo','nm_webcontact').'</a>';
+$proFeatures .= '</p>';
+
+
+$proFeatures .= '<br>More information contact: <a href="mailto:sales@najeebmedia.com">sales@najeebmedia.com</a>';
+
+echo $proFeatures;
+?>
